da328aae7b455b8250188221feb5c59a6fe71202
FIgrowing <126572361+FIgrowing@users.noreply.github.com>
2024-06-08 20:10:15 +0800

    加入分布式锁以及秒杀优惠券业务

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -41,54 +42,57 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     public Result seckillVoucher(Long voucherId) {
 
         SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
-        if (LocalDateTime.now().isBefore(seckillVoucher.getBeginTime()))
-        {
+        if (LocalDateTime.now().isBefore(seckillVoucher.getBeginTime())) {
             return Result.fail("秒杀尚未开始");
         }
 
-        if (LocalDateTime.now().isAfter(seckillVoucher.getEndTime()))
-        {
+        if (LocalDateTime.now().isAfter(seckillVoucher.getEndTime())) {
             return Result.fail("秒杀已结束");
         }
 
-        if (seckillVoucher.getStock()<1)
-        {
+        if (seckillVoucher.getStock() < 1) {
             return Result.fail("库存不足");
         }
 
         // todo 一人一单逻辑
         Long userId = UserHolder.getUser().getId();
-         //todo 由于createVoucherOrder()方法使用了事务注解，所以我们要拿到IVoucherOrderService的代理对象，否则事务失效
-            IVoucherOrderService proxy =(IVoucherOrderService) AopContext.currentProxy();
-            return proxy.createVoucherOrder(voucherId);
 
-        //
+        // todo 尝试获取分布式锁
+        RLock lock = redissonClient.getLock("lock:order:" + userId);
+        boolean isLock = lock.tryLock();
+        if (!isLock) {
+            return Result.fail("不允许重复下单");
+        }
+
+        try {
+            //todo 由于createVoucherOrder()方法使用了事务注解，所以我们要拿到IVoucherOrderService的代理对象，否则事务失效
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        } catch (IllegalStateException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
 
 
     }
 
     @Transactional
-    public Result createVoucherOrder(Long voucherId)
-    {
+    public Result createVoucherOrder(Long voucherId) {
 
         Long userId = UserHolder.getUser().getId();
 
         int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
         // 5.2.判断是否存在
 
-
-
-
         if (count > 0) {
             // 用户已经购买过了
-
             return Result.fail("不允许重复下单");
         }
         // 扣减库存
         boolean isSeccess = seckillVoucherService.update().setSql("stock = stock - 1")
                 .eq("voucher_id", voucherId).gt("stock", 0).update();
-        if (!isSeccess)
-        {
+        if (!isSeccess) {
             return Result.fail("库存不足");
         }
         // 创建订单
