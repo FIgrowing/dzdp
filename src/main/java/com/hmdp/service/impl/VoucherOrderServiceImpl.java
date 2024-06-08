f3ed659191a64c713ef2c3dd083c1f164a628a00
FIgrowing <126572361+FIgrowing@users.noreply.github.com>
2024-06-08 19:58:11 +0800

    完成优惠券秒杀和加单机锁

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,21 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.redisson.api.RedissonClient;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +28,78 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+
+        SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+        if (LocalDateTime.now().isBefore(seckillVoucher.getBeginTime()))
+        {
+            return Result.fail("秒杀尚未开始");
+        }
+
+        if (LocalDateTime.now().isAfter(seckillVoucher.getEndTime()))
+        {
+            return Result.fail("秒杀已结束");
+        }
+
+        if (seckillVoucher.getStock()<1)
+        {
+            return Result.fail("库存不足");
+        }
+
+        // todo 一人一单逻辑
+        Long userId = UserHolder.getUser().getId();
+         //todo 由于createVoucherOrder()方法使用了事务注解，所以我们要拿到IVoucherOrderService的代理对象，否则事务失效
+            IVoucherOrderService proxy =(IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+
+        //
+
+
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId)
+    {
+
+        Long userId = UserHolder.getUser().getId();
+
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        // 5.2.判断是否存在
+
+
+
+
+        if (count > 0) {
+            // 用户已经购买过了
+
+            return Result.fail("不允许重复下单");
+        }
+        // 扣减库存
+        boolean isSeccess = seckillVoucherService.update().setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId).gt("stock", 0).update();
+        if (!isSeccess)
+        {
+            return Result.fail("库存不足");
+        }
+        // 创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        voucherOrder.setVoucherId(voucherId);
+        voucherOrder.setUserId(userId);
+        save(voucherOrder);
+
+        return Result.ok(orderId);
+
+    }
 }
